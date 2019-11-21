ad52f220d0dd7780be3c9e1dfd8551019562e62b
Craig Stroberg <strobergcd@gmail.com>
2019-11-21 20:14:42 +0200

    Secret Santa

--- SecretSanta.java
+++ SecretSanta.java
@@ -3,23 +3,23 @@ import java.util.List;
 
 public class SecretSanta{
     public static void main(String[] args) {
-        List<String> buyer = List.of("Angela", "Kevin", "Justin", "Craig", "Gregory", "Nunny", "Drummond", "Andrea");
+        List<String> gifter = List.of("Angela", "Kevin", "Justin", "Craig", "Gregory", "Nunny", "Drummond", "Andrea");
         List<String> receiver = new ArrayList<>();
 
-        while (buyer.size() != receiver.size()) {
-            double x = (Math.random() * ((buyer.size()) + 1));
+        while (gifter.size() != receiver.size()) {
+            double x = (Math.random() * ((gifter.size()) + 1));
             final int receiverIndex = Double.valueOf(x).intValue();
-            if (receiverIndex < buyer.size()) {
-                final String receiverName = buyer.get(receiverIndex);
-                if ((buyer.indexOf(receiverName) != receiver.size()) && !receiver.contains(receiverName)) {
+            if (receiverIndex < gifter.size()) {
+                final String receiverName = gifter.get(receiverIndex);
+                if ((gifter.indexOf(receiverName) != receiver.size()) && !receiver.contains(receiverName)) {
                     receiver.add(receiverName);
                 }
             }
         }
 
         System.out.println("Merry Chirstmas - Lets play secret Santa");
-        for (int i = 0; i < buyer.size(); i++) {
-            System.out.printf("Buyer: %-20s Receiver: %s%n", buyer.get(i), receiver.get(i));
+        for (int i = 0; i < gifter.size(); i++) {
+            System.out.printf("Gifter: %-20s Receiver: %s%n", gifter.get(i), receiver.get(i));
         }
     }
 }
